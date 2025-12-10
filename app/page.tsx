0245f90094323a497ec6125752aa7e1626341355
Yaypixel <thurstonreese09@outlook.com>
2025-12-10 15:44:36 -0500

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -39,13 +39,6 @@ export default function Home() {
      <ScrollReveal>
         <div className="hero container relative w-500 mx-auto pt-10 z-10" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
           <ParallaxHover strength={0} zoom={isHovered ? 1.12 : 1} className="relative rounded-lg">
-            <FlickeringGrid className={`z-10 absolute inset-0 size-full ${isHovered ? 'opacity-0' : ''}`}
-            squareSize={7}
-            gridGap={10}
-            color="#3182ce"
-            maxOpacity={0.4}
-            flickerChance={1}
-            />
             <img src="/BBOTS2025.jpg" className="w-full h-full object-cover"></img>
             <div className={`absolute inset-0  ${isHovered ? 'bg-transparent' : 'bg-blue-400/50'} transition-all duration-200`}></div>
             
--- components/ui/info-block.tsx
+++ components/ui/info-block.tsx
@@ -26,20 +26,13 @@ export default function InfoBlock({
     <div className={"hero container relative w-max h-max mx-auto pt-10 z-10 overflow-hidden" + className}  onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
         
         <ParallaxHover strength={0} zoom={isHovered ? 1.12 : 1} className="relative rounded-lg">
-            <FlickeringGrid className={`z-10 absolute inset-0 size-full ${isHovered ? 'opacity-0' : ''}`}
-                squareSize={4}
-                gridGap={6}
-                color="#3182ce"
-                maxOpacity={0.4}
-                flickerChance={1}
-                />
                 <div className={`inset-0 transition-all duration-300 ${isHovered ? "brightness-75 scale-105" : "brightness-100 scale-100"}`}>
                     {children}
                 </div>
                 <div className={`absolute inset-0  ${isHovered ? 'bg-blue-500/50 -z-50' : 'bg-blue-400/50'} transition-all duration-200`}></div>
                 <div className={`absolute inset-0 flex items-center justify-center ${isHovered ? '-z-50' : ""}`}>
-                  <h2 className={`bitcount-prop-single-ocebots text-8xl text-white ${isHovered ? "opacity-100 scale-110" : "opacity-60 scale-100"}  transition-all duration-200`}>{header}</h2>
-            </div>
+                  <h2 className={`bitcount-prop-single-ocebots text-8xl text-white ${isHovered ? "scale-110" : "scale-100"}  opacity-100 transition-all duration-200`}>{header}</h2>
+                </div>
         </ParallaxHover>
     </div>
     </a>
